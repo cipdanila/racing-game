e61385fd15c239591dce3484bc7eb550cf71b5a9
Ciprian Danila <ciprian.danila@yahoo.com>
2023-03-16 16:03:29 +0200

    added Car class; studied variables

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,13 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String name = "test";
+    String color;
+    int doorCount;
+    double fuelLevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+    boolean damaged;
+}
