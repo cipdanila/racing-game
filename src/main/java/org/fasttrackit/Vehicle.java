022369f56e8308d903a5821c7c17e61812c1a293
Ciprian Danila <ciprian.danila@yahoo.com>
2023-03-20 17:15:28 +0200

    declaring methods

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -10,4 +10,20 @@ public class Vehicle {
     double totalTraveledDistance;
     double maxSpeed;
     boolean damaged;
+
+    public double accelerate(double speed, double durationInHours) {
+            // Ferrari is accelerating with 100 km/h for 1h
+        System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + "h");
+
+        double distance = speed * durationInHours;
+
+        totalTraveledDistance = totalTraveledDistance + distance; // totalTraveledDistance += distance;
+        System.out.println("Total traveled distance for vehicle " + name + ": " + totalTraveledDistance);
+
+        double usedFuel = distance * mileage / 100;
+        fuelLevel -= usedFuel;
+        System.out.println("Remaining fuel for vehicle " + name + ": " + fuelLevel);
+
+        return distance;
+    }
 }
