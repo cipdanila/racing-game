7df426f35d3dac476e5f8c181a30003a39907ce7
Ciprian Danila <ciprian.danila@yahoo.com>
2023-03-21 15:07:36 +0200

    conditional statements (if...else)

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -12,7 +12,23 @@ public class Vehicle {
     boolean damaged;
 
     public double accelerate(double speed, double durationInHours) {
-            // Ferrari is accelerating with 100 km/h for 1h
+
+        if (fuelLevel <= 0 || damaged) {
+                // "|" -booth expressions are evaluated even that the first expression is true
+                // "||" -stopped if first expression is true
+            System.out.println("You cannot accelerate!");
+            return 0;                       // exit accelerate() method
+        }
+
+        if (speed > maxSpeed) {
+            System.out.println("Max speed exceeded!");
+            return 0;                       // exit accelerate() method
+        } else if (speed == maxSpeed) {
+            System.out.println("Careful, max speed reached.");
+        } else {
+            System.out.println("Valid speed entered.");
+        }
+
         System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + "h");
 
         double distance = speed * durationInHours;
@@ -20,7 +36,13 @@ public class Vehicle {
         totalTraveledDistance = totalTraveledDistance + distance; // totalTraveledDistance += distance;
         System.out.println("Total traveled distance for vehicle " + name + ": " + totalTraveledDistance);
 
-        double usedFuel = distance * mileage / 100;
+        double mileageMultiplier = 1;
+
+        if (speed > 120) {
+            mileageMultiplier = speed / 100;
+        }
+
+        double usedFuel = distance * mileage * mileageMultiplier / 100;
         fuelLevel -= usedFuel;
         System.out.println("Remaining fuel for vehicle " + name + ": " + fuelLevel);
 
